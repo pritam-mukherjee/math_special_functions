2495b5a29a364d617056746ce3473041799166c7
Pritam Mukherjee <mukherjeepritam02@gmail.com>
2020-03-24 23:41:00 +0530

    Fixed the tab indexing in sigmoid function file

--- sigmoid/sigmoid.cpp
+++ sigmoid/sigmoid.cpp
@@ -3,23 +3,23 @@
 
 using namespace std;
 
-_Float64x sigm(float num)
+long double sigm(float num)
 {
-    _Float64 ex = exp(num);
-    return (ex/(ex+1));
+	long double  ex = exp(num);
+    	return (ex/(ex+1));
 }
 
 int main()
 {
-    cout << "Enter a number: ";
-    float number;
-    cin >> number;
-    if(number > 88)
-    {
-        cout << "Sigmoid val = 1" << endl;
-        return 0;
-    }
-    cout << "Sigmoid val = " << sigm(number) << endl;
+    	cout << "Enter a number: ";
+    	float number;
+    	cin >> number;
+	if(number > 88)
+    	{
+        	cout << "Sigmoid val = 1" << endl;
+        	return 0;
+    	}
+    	cout << "Sigmoid val = " << sigm(number) << endl;
 
-    return 0;
+    	return 0;
 }
